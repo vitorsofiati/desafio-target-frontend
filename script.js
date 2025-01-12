45ec998ee49771c0e845cf26ed9b57ce84f409f4
Vitor <vitor.sofiati@live.com>
2025-01-12 12:26:02 -0300

    desafio extra realizado

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ import { exercicio2 } from './js/exercicio2.js';
 import { exercicio3 } from './js/exercicio3.js';
 import { exercicio4 } from './js/exercicio4.js';
 import { exercicio5 } from './js/exercicio5.js';
+import { extra } from './js/extra.js';
 
 // Exibir o resultado na tela
 function exibirExercicio(enunciado, resultado) {
@@ -16,3 +17,4 @@ window.executarExercicio2 = () => exercicio2(exibirExercicio);
 window.executarExercicio3 = () => exercicio3(exibirExercicio);
 window.executarExercicio4 = () => exercicio4(exibirExercicio);
 window.executarExercicio5 = () => exercicio5(exibirExercicio);
+window.executarExtra = () => extra(exibirExercicio);
